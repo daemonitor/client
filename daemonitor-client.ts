87b25a83f47db42089ba9df6b8a48bd866d907c8
Larry W <l422y@l422y.com>
2023-04-15 15:59:51 -0400

    chore: add startup message

--- daemonitor-client.ts
+++ daemonitor-client.ts
@@ -11,6 +11,7 @@ const systemKey = process.env.SYSTEM_KEY || "unknown"
 const apiBaseUrl = process.env.API_BASE_URL === "production" ? "http://app.daemonitor.com/api" : "http://localhost:5678/api"
 const apiURL = `${apiBaseUrl}/clientstate/update`
 
+console.log(`Daemonitor client starting up for ${myHostname} with key ${systemKey}, using API at ${apiURL}`)
 pm2.connect(function (err) {
     if (err) {
         console.error(err)
