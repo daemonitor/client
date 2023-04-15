fdf6b0b1b699145c40100461c7d11f720ca52122
Larry W <l422y@l422y.com>
2023-04-15 14:38:52 -0400

    chore: adjust for typescript

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,8 @@ module.exports = {
     apps: [
         {
             name: "daemonitor-client",
-            script: "./daemonitor-client.js",
+            script: "./daemonitor-client.ts",
             watch: true,
-            instance_var: 'INSTANCE_ID',
-            env: {
-                "PORT": 3000,
-                "NODE_ENV": "production"
-            }
         }
     ]
 }
