dc2751f8511dc4b39d1ff6a00a30371e93414af6
Larry W <l422y@l422y.com>
2023-04-18 05:15:44 -0400

    Add alias and description properties to MonitoringPluginBase constructor to provide more information about the plugin

--- lib/MonitoringPluginBase.ts
+++ lib/MonitoringPluginBase.ts
@@ -3,19 +3,22 @@ import PluginManager from "./PluginManager"
 
 abstract class MonitoringPluginBase {
     static currentPluginIndex = 0
+    protected alias: string
     protected name: string
+    protected description: string
     protected instanceName: string
     protected refreshTimer: any
     protected uniqueId: string
     protected config: any
     protected pluginIndex: number
 
-    protected constructor(name: string) {
+    protected constructor(alias: string, name: string, description: string) {
         this.pluginIndex = MonitoringPluginBase.currentPluginIndex++
-        this.config = ConfigProvider.get(name)
+        this.config = ConfigProvider.get(alias)
         this.name = name
+        this.description = description
         this.instanceName = this.config.name || name
-        this.uniqueId = this.config?.uniqueId || this.name + "-" + this.pluginIndex
+        this.uniqueId = this.config?.uniqueId || this.alias + "-" + this.pluginIndex
     }
 
     public async send(data: any, unique_id?: string): Promise<void> {
