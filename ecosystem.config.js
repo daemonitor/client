6ec935953831084941c36541d549fda3cfd1ff8a
Larry W <l422y@l422y.com>
2023-04-18 03:30:05 -0400

    Refactor ecosystem.config.js to include the ts-node interpreter for the daemonitor-client script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    apps: [
-        {
-            name: "daemonitor-client",
-            script: "./daemonitor-client.ts",
-            watch: true,
-        }
-    ]
+    apps: [{
+        name: "daemonitor-client",
+        interpreter: "ts-node",
+        script: "./daemonitor-client.ts",
+        watch: true,
+    }]
 }
