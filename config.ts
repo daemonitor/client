c2dbe5dbd9c4480b9f9d30643fe9b676587227e2
Larry W <l422y@l422y.com>
2023-04-22 20:32:56 -0400

    Add 'web' plugin to the config file and define its endpoints

--- config.ts
+++ config.ts
@@ -1,11 +1,18 @@
 export default {
-    "plugins": [
+    plugins: [
         "pm2",
-        "os"
+        "os",
+        "web"
     ],
-    "pm2": {
-        "name": "pm2",
-        "uniqueId": "pm2",
-        "refreshInterval": 10000
+    pm2: {
+        name: "pm2",
+        uniqueId: "pm2",
+        refreshInterval: 10000
+    },
+    web: {
+        endpoints: [
+            {"name": "L422Y.COM", "url": "https://l422y.com", expectedStrings: ["l422y"]},
+            {"name": "CITYRFP STAGE", "url": "https://stage.cityrfp.com", unexpectedStrings: ["Bad Gateway"]},
+        ]
     }
 }
