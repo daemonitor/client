d2e04dd9eb3e9d06839b6a2e9cabd773ad14e3d2
Larry W <l422y@l422y.com>
2023-04-22 20:38:42 -0400

    Create a new module "@daemonitor/client" with an IConnector interface and a commented out RestApiConnector class that implements it

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+declare module "@daemonitor/client" {
+    import { IConnector } from "@daemonitor/common"
+
+    // export interface RestApiConnector implements IConnector {
+    //     constructor(apiUrl: string, systemKey: string)
+    //
+    //     sendData(data: any, type: string, uniqueId: string): Promise<void>
+    // }
+}
